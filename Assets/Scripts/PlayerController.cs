4fae48d6dadd899c45bf5f38d7814cb36086bd1d
yuurekuji <Kanoyxng@gmail.com>
2025-11-17 17:03:08 -0500

    Task 1 complete
    Finished task 1: Added modular speed stat for movement and utilized the directions as a player Input. Everything works, just lacks jumping and vertical movement which is not covered or required this week.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Build.Player;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public Rigidbody2D rb2D;
+    public float moveSpeed;
+    private Vector2 moveDir;
     public enum FacingDirection
     {
         left, right
@@ -12,7 +18,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -21,12 +27,20 @@ public class PlayerController : MonoBehaviour
         //The input from the player needs to be determined and then passed in the to the MovementUpdate which should
         //manage the actual movement of the character.
         Vector2 playerInput = new Vector2();
+
+        float moveX = Input.GetAxisRaw("Horizontal");
+        playerInput = new Vector2(moveX, 0).normalized;
+
         MovementUpdate(playerInput);
+
+
+
+
     }
 
     private void MovementUpdate(Vector2 playerInput)
     {
-
+        rb2D.linearVelocity = new Vector2(playerInput.x * moveSpeed, playerInput.y * moveSpeed);
     }
 
     public bool IsWalking()
