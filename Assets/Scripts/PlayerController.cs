a71bd43cb61fd9b790b0fbc0e106e5f799135ff9
yuurekuji <Kanoyxng@gmail.com>
2025-11-17 13:48:31 -0500

    Added Package to project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public enum FacingDirection
+    {
+        left, right
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //The input from the player needs to be determined and then passed in the to the MovementUpdate which should
+        //manage the actual movement of the character.
+        Vector2 playerInput = new Vector2();
+        MovementUpdate(playerInput);
+    }
+
+    private void MovementUpdate(Vector2 playerInput)
+    {
+
+    }
+
+    public bool IsWalking()
+    {
+        return false;
+    }
+    public bool IsGrounded()
+    {
+        return true;
+    }
+
+    public FacingDirection GetFacingDirection()
+    {
+        return FacingDirection.left;
+    }
+}
---
+++ Assets/Scripts/PlayerVisuals.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script manages updating the visuals of the character based on the values that are passed to it from the PlayerController.
+/// NOTE: You shouldn't make changes to this script when attempting to implement the functionality for the W10 journal.
+/// </summary>
+public class PlayerVisuals : MonoBehaviour
+{
+    public Animator animator;
+    public SpriteRenderer bodyRenderer;
+    public PlayerController playerController;
+
+    private int isWalkingHash, isGroundedHash;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isWalkingHash = Animator.StringToHash("IsWalking");
+        isGroundedHash = Animator.StringToHash("IsGrounded");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        VisualsUpdate();
+    }
+
+    //It is not recommended to make changes to the functionality of this code for the W10 journal.
+    private void VisualsUpdate()
+    {
+        animator.SetBool(isWalkingHash, playerController.IsWalking());
+        animator.SetBool(isGroundedHash, playerController.IsGrounded());
+        switch (playerController.GetFacingDirection())
+        {
+            case PlayerController.FacingDirection.left:
+                bodyRenderer.flipX = true;
+                break;
+            case PlayerController.FacingDirection.right:
+            default:
+                bodyRenderer.flipX = false;
+                break;
+        }
+    }
+}
